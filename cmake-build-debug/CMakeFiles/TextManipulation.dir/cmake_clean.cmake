06171c07e371b924c480da8bd19a0e625396066a
Muhammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2021-04-17 01:39:50 +0430

    initializing master

---
+++ cmake-build-debug/CMakeFiles/TextManipulation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextManipulation.dir/main.cpp.o"
+  "TextManipulation"
+  "TextManipulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TextManipulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
