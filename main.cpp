3bb5d161ab584165daa73c69ebb588549492f173
Muhammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2021-04-17 14:46:06 +0430

    making header

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-
+#include "text.h"
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    
 }
---
+++ text.h
@@ -0,0 +1,8 @@
+//
+// Created by mksht80 on 4/17/21.
+//
+//
+//#ifndef TEXTMANIPULATION_TEXT_H
+//#define TEXTMANIPULATION_TEXT_H
+//
+//#endif //TEXTMANIPULATION_TEXT_H
