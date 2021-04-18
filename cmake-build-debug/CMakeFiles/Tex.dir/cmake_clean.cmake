b9f6042b4a4a9f5b04b3badd9cac5e94306f1363
Muhammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2021-04-18 10:24:34 +0430

    porject is done

---
+++ cmake-build-debug/CMakeFiles/Tex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tex.dir/main.cpp.o"
+  "Tex"
+  "Tex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- text.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by mksht80 on 4/17/21.
-//
-//
-//#ifndef TEXTMANIPULATION_TEXT_H
-//#define TEXTMANIPULATION_TEXT_H
-//
-//#endif //TEXTMANIPULATION_TEXT_H
